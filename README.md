b5a99f51ace44d7716b3ead46e557a34e87d1ceb
David Todd <me@david.to>
2020-06-23 12:09:23 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# gov-uk-cookies-banner
+# GOV.UK cookies banner example
+
+## Areas with code to reuse
+
+Each of the following areas contain the code you need to replicate the banner in your prototype. The areas to copy are highlighted with code comments.
+
+- layout.html
+- application.js
+- applications.scss
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,79 @@
+/* global $ */
+
+// Warn about using the kit in production
+if (window.console && window.console.info) {
+	window.console.info('GOV.UK Prototype Kit - do not use for production')
+}
+
+$(document).ready(function () {
+	window.GOVUKFrontend.initAll()
+})
+
+// COOKIE BANNER START
+
+function initializeAnalytics() {
+	var acceptAllCookies = localStorage.getItem('acceptAllCookies')
+	if (acceptAllCookies == 'true') {
+		window.dataLayer = window.dataLayer || []
+		function gtag() {
+			dataLayer.push(arguments)
+		}
+		gtag('js', new Date())
+		gtag('config', 'UA-29011943-12')
+	}
+}
+
+initializeAnalytics()
+
+function deleteAllCookies() {
+	var cookies = document.cookie.split(';')
+
+	for (var i = 0; i < cookies.length; i++) {
+		var cookie = cookies[i]
+		var eqPos = cookie.indexOf('=')
+		var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie
+		document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT'
+	}
+}
+
+function processCookiesToggle() {
+	var acceptAllCookies = window.localStorage.getItem('acceptAllCookies')
+	if (acceptAllCookies == 'true') {
+		$('.toggle-cookies .accept-cookies').hide()
+		$('.toggle-cookies .functional-cookies').show()
+	} else {
+		$('.toggle-cookies .accept-cookies').show()
+		$('.toggle-cookies .functional-cookies').hide()
+	}
+}
+
+function saveCookieChoice(allow) {
+	window.localStorage.setItem('acceptAllCookies', allow)
+	window.localStorage.setItem('seenCookieBanner', true)
+	if (!allow) {
+		deleteAllCookies()
+	}
+	var $cookieBanner = $('.casa-cookie-banner')
+	$cookieBanner.slideUp('fast')
+	processCookiesToggle()
+	initializeAnalytics()
+}
+
+function processCookieBanner() {
+	var seenCookieBanner = window.localStorage.getItem('seenCookieBanner')
+	if (seenCookieBanner == null) {
+		window.localStorage.setItem('seenCookieBanner', 'false')
+	}
+	var $cookieBanner = $('.casa-cookie-banner')
+	if (!(seenCookieBanner == 'true')) {
+		$('body').prepend($cookieBanner)
+		$cookieBanner.css('display', 'flex')
+	} else {
+		$cookieBanner.remove()
+	}
+	processCookiesToggle()
+}
+
+processCookieBanner()
+
+// COOKIE BANNER END
